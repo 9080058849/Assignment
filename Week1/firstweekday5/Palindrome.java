b0d0c5bdacd8d9fe88e567a01d7f764a95fe7509
Mahesh M <82584871+9080058849@users.noreply.github.com>
2022-02-14 19:04:58 +0530

    Update Palindrome.java

--- Week1/firstweekday5/Palindrome.java
+++ Week1/firstweekday5/Palindrome.java
@@ -8,16 +8,13 @@ public class Palindrome
 	public static void main(String[] args) 
 	{
 		String name="madam";
-		String reva="";
-		//int val=name.length();
-		char[] reverse=name.toCharArray();
-		/*for(int i=val-1;i>=0;i--) {
-			System.out.println(val[0]);
-		}*/
-		for (int i =reverse.length-1; i>=0; i--) {
-			String rev=reverse.toString();
-			}
-		    System.out.print(reverse);
+		String rev="";
+		for (int i =name.length()-1; i>=0; i--)
+		  {
+			name.charAt(i);
+			rev=rev+name.charAt(i);
+	          }
+		
 		    if(name.equalsIgnoreCase(rev))
 		{
 			System.out.println("It is a palindrome");
@@ -25,5 +22,6 @@ public class Palindrome
 		else 
 			System.out.println("It is not a palindrome");
 	}
+	
 }    
 
